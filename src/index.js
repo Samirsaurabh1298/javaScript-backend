54f118c3a07e1c5297049e3163007669c77007f8
Samirsaurabh <saurabh.samir@mahindra.com>
2024-05-11 23:52:02 +0530

    mongodb connection setup done

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+/*
+(async () => {
+  try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+      app.on('error', (error) => {
+          console.log("Error: ", error);
+          throw error;
+      })
+    
+      app.listen(process.env.PORT, () => {
+          console.log(`App is listening on port ${process.env.PORT}`);
+      })
+
+  } catch (error) {
+    console.log("ERROR:", error);
+    throw error;
+  }
+})();
+*/
